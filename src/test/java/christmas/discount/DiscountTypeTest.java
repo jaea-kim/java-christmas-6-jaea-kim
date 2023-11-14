bd2276a7ab2255f7f70bf72bd33ea1d9b184c60c
jaea-kim <jaeakim98@gmail.com>
2023-11-15 08:03:13 +0900

    TEST: 할인 연산 테스트 코드 작성

--- src/test/java/christmas/discount/DiscountTypeTest.java
+++ src/test/java/christmas/discount/DiscountTypeTest.java
@@ -1,9 +1,14 @@
 package christmas.discount;
 
+import christmas.config.Delimiter;
+import christmas.config.EventConstants;
+import christmas.order.Menu;
+import christmas.order.Order;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 class DiscountTypeTest {
@@ -55,5 +60,65 @@ class DiscountTypeTest {
         List<DiscountType> discountTypes = DiscountType.getDiscountTypes(dateDTO);
 
         Assertions.assertThat(prediction.containsAll(discountTypes)).isTrue();
+    }
+
+    @Test
+    @DisplayName("평일 할인에 디저트 메뉴가 없다면 평일 할인 금액은 0이 되어야한다.")
+    void calculate_weekday_no_dessert() {
+        String menu1 = getOrderMenuString(Menu.BBQ_RIBS, 1);
+        String menu2 = getOrderMenuString(Menu.TAPAS, 3);
+        LocalDate weekday = getLocalDate(4);
+        Order order = new Order(weekday, new String[]{menu1, menu2});
+
+        Assertions.assertThat(DiscountType.WEEKDAY.calculateDiscountAmount(order)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("평일 할인에 디저트 메뉴가 여러 개라면 평일 할인 금액은 2023 x 개수가 되어야한다.")
+    void calculate_weekday_dessert() {
+        int quantity = 2;
+        String menu1 = getOrderMenuString(Menu.ICE_CREAM, quantity);
+        LocalDate weekday = getLocalDate(4);
+        Order order = new Order(weekday, new String[]{menu1});
+
+        Assertions.assertThat(DiscountType.WEEKDAY.calculateDiscountAmount(order)).isEqualTo(2023 * quantity);
+    }
+
+    @Test
+    @DisplayName("크리스마스 디데이 할인은 1일에 할인 금액은 1000원이 되어야한다.")
+    void calculate_christmas_one() {
+        int quantity = 2;
+        String menu1 = getOrderMenuString(Menu.ICE_CREAM, quantity);
+        String menu2 = getOrderMenuString(Menu.TAPAS, quantity);
+        LocalDate weekend = getLocalDate(1);
+        Order order = new Order(weekend, new String[]{menu1, menu2});
+
+        List<DiscountType> types = DiscountType.getDiscountTypes(new DateDTO(CustomDay.FRIDAY, 1));
+        int index = types.indexOf(DiscountType.CHRISTMAS);
+
+        Assertions.assertThat(types.get(index).calculateDiscountAmount(order)).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("크리스마스 디데이 할인은 25일에 할인 금액은 3400원이 되어야한다.")
+    void calculate_christmas() {
+        int quantity = 2;
+        String menu1 = getOrderMenuString(Menu.ICE_CREAM, quantity);
+        String menu2 = getOrderMenuString(Menu.TAPAS, quantity);
+        LocalDate weekday = getLocalDate(25);
+        Order order = new Order(weekday, new String[]{menu1, menu2});
+
+        List<DiscountType> types = DiscountType.getDiscountTypes(new DateDTO(CustomDay.MONDAY, 25));
+        int index = types.indexOf(DiscountType.CHRISTMAS);
+
+        Assertions.assertThat(types.get(index).calculateDiscountAmount(order)).isEqualTo(3400);
+    }
+
+    private String getOrderMenuString(Menu menu, int quantity) {
+        return menu.getLabel() + Delimiter.MENU.getSymbol() + quantity;
+    }
+
+    private LocalDate getLocalDate(int date) {
+        return LocalDate.of(EventConstants.EVENT_YEAR.getValue(), EventConstants.EVENT_MONTH.getValue(), date);
     }
 }
