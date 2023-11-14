15a99af69477638fbf467e626be37935dd97c299
jaea-kim <jaeakim98@gmail.com>
2023-11-14 16:23:39 +0900

    FEAT: 메뉴 입력 후 패턴 확인

--- src/main/java/christmas/EventController.java
+++ src/main/java/christmas/EventController.java
@@ -1,10 +1,12 @@
 package christmas;
 
 import christmas.config.EventMessage;
+import christmas.domain.OrderMenu;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class EventController {
     private final InputView inputView;
@@ -22,12 +24,23 @@ public class EventController {
     private void promptForReservation() {
         outputView.printMessage(EventMessage.GREETINGS.getMessage());
         LocalDate date = reservationForDate();
+        String[] orderMenus = reservationForMenu();
     }
 
     private LocalDate reservationForDate() {
         while (true) {
             try {
                 return inputView.readDate();
+            } catch (IllegalArgumentException e) {
+                outputView.printMessage(e.getMessage());
+            }
+        }
+    }
+
+    private String[] reservationForMenu() {
+        while (true) {
+            try {
+                return inputView.readOrderMenus();
             } catch (IllegalArgumentException e) {
                 outputView.printMessage(e.getMessage());
             }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,14 +1,18 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.config.Delimiter;
 import christmas.config.ErrorMessage;
 import christmas.config.EventConstants;
 import christmas.config.EventMessage;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class InputView {
+    public static final String MENU_REGEX = "^[a-zA-Z]+" + Delimiter.MENU.getSymbol() + "[0-9]+$";
+
     public LocalDate readDate() {
         try {
             System.out.println(EventMessage.ASK_EATING_OUT.getMessage());
@@ -20,5 +24,20 @@ public class InputView {
         } catch (NumberFormatException | DateTimeException e) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DATE.getMessage());
         }
+    }
+
+    public String[] readOrderMenus() {
+        System.out.println(EventMessage.ASK_TO_ORDER);
+        String[] input = Console.readLine().split(Delimiter.ORDER.getSymbol());
+
+        if (validateOrderPattern(input)) {
+            return input;
+        }
+
+        throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.getMessage());
+    }
+
+    private boolean validateOrderPattern(String[] input) {
+        return Arrays.stream(input).allMatch(i -> i.matches(MENU_REGEX));
     }
 }
