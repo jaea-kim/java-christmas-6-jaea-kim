1ac3982db48793a9e70b480550a09825cb94f6f2
jaea-kim <jaeakim98@gmail.com>
2023-11-14 20:22:18 +0900

    FEAT: 이벤트 상수 추가

--- src/main/java/christmas/config/EventConstants.java
+++ src/main/java/christmas/config/EventConstants.java
@@ -2,7 +2,10 @@ package christmas.config;
 
 public enum EventConstants {
     EVENT_YEAR(2023),
-    EVENT_MONTH(12);
+    EVENT_MONTH(12),
+    MIN_QUANTITY(1),
+    MAX_ALL_QUANTITY(20),
+    MIN_EVENT_AMOUNT(10000);
 
     private final int value;
 
