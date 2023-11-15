34b9cc342ccd8c3fa2f5e767139d408aaf757ade
jaea-kim <jaeakim98@gmail.com>
2023-11-15 14:32:50 +0900

    REFACTOR: OrderMenus 생성자 시그니처 변경에 따른 테스트 수정

--- src/test/java/christmas/order/OrderMenusTest.java
+++ src/test/java/christmas/order/OrderMenusTest.java
@@ -6,13 +6,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class OrderMenusTest {
     @Test
     @DisplayName("메뉴가 중복될 경우 IllegalArgumentException 예외가 발생한다.")
     void menus_duplicate() {
         String menu = Menu.BBQ_RIBS.getLabel();
-        String orderMenu = menu + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
-        String[] orderMenus = new String[]{orderMenu, orderMenu};
+        String orderMenuString = menu + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
+        OrderMenu orderMenu = new OrderMenu(orderMenuString);
+        List<OrderMenu> orderMenus = List.of(orderMenu, orderMenu);
 
         Assertions.assertThatThrownBy(() -> new OrderMenus(orderMenus))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -22,8 +25,9 @@ class OrderMenusTest {
     @DisplayName("음료 메뉴만 주문할 경우 IllegalArgumentException 예외가 발생한다.")
     void menus_only_beverage() {
         String menu = Menu.CHAMPAGNE.getLabel();
-        String orderMenu = menu + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
-        String[] orderMenus = new String[]{orderMenu};
+        String orderMenuString = menu + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
+        OrderMenu orderMenu = new OrderMenu(orderMenuString);
+        List<OrderMenu> orderMenus = List.of(orderMenu, orderMenu);
 
         Assertions.assertThatThrownBy(() -> new OrderMenus(orderMenus))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -32,9 +36,11 @@ class OrderMenusTest {
     @Test
     @DisplayName("음료 메뉴만 여러 종류 주문할 경우 IllegalArgumentException 예외가 발생한다.")
     void menus_only_beverage_two() {
-        String orderMenu1 = Menu.CHAMPAGNE.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
-        String orderMenu2 = Menu.RED_WINE.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
-        String[] orderMenus = new String[]{orderMenu1, orderMenu2};
+        String orderMenuString1 = Menu.CHAMPAGNE.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
+        String orderMenuString2 = Menu.RED_WINE.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MIN_QUANTITY.getValue();
+        OrderMenu orderMenu1 = new OrderMenu(orderMenuString1);
+        OrderMenu orderMenu2 = new OrderMenu(orderMenuString2);
+        List<OrderMenu> orderMenus = List.of(orderMenu1, orderMenu2);
 
         Assertions.assertThatThrownBy(() -> new OrderMenus(orderMenus))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -43,12 +49,13 @@ class OrderMenusTest {
     @Test
     @DisplayName("총 메뉴 수량이 최대 수량을 넘은 경우 IllegalArgumentException 예외가 발생한다.")
     void menus_over_max_quantity() {
-        String orderMenu1 = Menu.CHAMPAGNE.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MAX_ALL_QUANTITY.getValue();
-        String orderMenu2 = Menu.BBQ_RIBS.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MAX_ALL_QUANTITY.getValue();
-        String[] orderMenus = new String[]{orderMenu1, orderMenu2};
+        String orderMenuString1 = Menu.CHAMPAGNE.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MAX_ALL_QUANTITY.getValue();
+        String orderMenuString2 = Menu.BBQ_RIBS.getLabel() + Delimiter.MENU.getSymbol() + EventConstants.MAX_ALL_QUANTITY.getValue();
+        OrderMenu orderMenu1 = new OrderMenu(orderMenuString1);
+        OrderMenu orderMenu2 = new OrderMenu(orderMenuString2);
+        List<OrderMenu> orderMenus = List.of(orderMenu1, orderMenu2);
 
         Assertions.assertThatThrownBy(() -> new OrderMenus(orderMenus))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
 }
