8b81452cb808731aff056f89b2b8664f527ba03c
jaea-kim <jaeakim98@gmail.com>
2023-11-15 15:30:04 +0900

    FIX: 해당 배지의 최대값 추가하여 반환하도록 수정

--- src/main/java/christmas/discount/domain/Badge.java
+++ src/main/java/christmas/discount/domain/Badge.java
@@ -1,27 +1,29 @@
 package christmas.discount.domain;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
-public enum Badge {
-    STAR("별", 5000),
-    TREE("트리", 10000),
-    SANTA("산타", 20000),
-    NONE("없음", 0);
+public enum Badge implements Comparable<Badge> {
 
+    STAR("별", 5000, 10000),
+    TREE("트리", 10000, 20000),
+    SANTA("산타", 20000, Integer.MAX_VALUE),
+    NONE("없음", 0, 5000);
     private final String label;
-    private final int amount;
+    private final int minAmount;
+    private final int maxAmount;
 
-    Badge(String label, int amount) {
+    Badge(String label, int minAmount, int maxAmount) {
         this.label = label;
-        this.amount = amount;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
     }
 
     public static Badge getBadgeByAmount(int amount) {
-        return Arrays.stream(Badge.values())
-                .filter(badge -> amount >= badge.amount)
-                .reduce((first, second) -> second)
+        return Arrays.stream(values())
+                .filter(badge -> amount >= badge.minAmount && amount < badge.maxAmount)
+                .findFirst()
                 .orElse(NONE);
-
     }
 
     public String getLabel() {
